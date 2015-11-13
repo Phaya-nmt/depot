e42b3a6c8248c062d81b8c8e190c840596d11709
Phaya-nmt <tipherreth@hotmail.com>
2015-11-13 06:41:35 +0000

    get counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,36 @@
 class StoreController < ApplicationController
   before_action :now
 
+# if session[:counter].nil
+# end
+
 
   def index
     @products = Product.order(:title)
+
+
+
+puts "----------------------"
+puts session[:counter]
+puts "----------------------"
+    if session[:counter].nil?
+      session[:counter] = 1
+      else
+      session[:counter] += 1
+    end
+      session[:counter] = @counter
   end
+
+# if session != nil
+#   session['count'] = session['count'].to_i + 1
+# else
+#   session['count'] += 1
+#   session['count'] = @count
+#   session.close
+# end
+
+
+
+
+
 end
